1e78d702ebce794e143621b1f61da504cc3aa152
Grey Ruessler <greyruessler@gmail.com>
2020-12-23 14:16:48 -0600

    cards test

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,9 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   constructor() { }
+
+  sampleContent = [
+    {title:"Sample Title", type:"link", image:"asdf.png"},
+    {title:"Sample Title 2", type:"text", content:"asdfqwerty"}
+  ];
 
   ngOnInit(): void {
   }
