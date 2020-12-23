492de2a06fc5fd25da4ad2364e2cbee152dafa0d
Grey Ruessler <greyruessler@gmail.com>
2020-12-23 16:17:07 -0600

    scss migration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
