578ac6d5d298a3c43bca7601ca181bc4f269996c
Grey Ruessler <greyruessler@gmail.com>
2020-12-28 22:02:47 -0600

    firebase environment added

---
+++ src/environments/environment.fb.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    subredditLimit:25,
+    clientId:"J7qaQ26QYFcKEg",
+    authorizationType:"authorization_code",
+    refreshType:"refresh_token",
+    tokenEndpoint:"https://www.reddit.com/api/v1/access_token",
+    redirectUrl: "https://redditsharp-b7de3.firebaseapp.com/authenticate", //replace
+    scope:"read identity mysubreddits"
+  };
+  
